5f65aaa00ddcf7c3944115c2bc143a8ac170868f
PraveenGokavarapu12 <praveengokavarapu19@gmail.com>
2025-03-27 17:08:24 +0530

    Implemented lazyloading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,42 @@
-import React from 'react'
-import { BrowserRouter,Routes, Route,Navigate } from 'react-router-dom'
-import Signup from './pages/Signup'
-import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
-import UserDetails from './pages/UserDetails'
-import Navbar from './pages/Navbar'
-import PrivateRoute from './pages/PrivateRoute'
-import MyPosts from './pages/MyPosts'
-import LikedPosts from './pages/LikedPosts'
-import CreatePosts from './pages/CreatePosts'
-import Posts from './pages/Posts'
-import LandingPage from './pages/LandingPage'
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './pages/Navbar';
+import PrivateRoute from './pages/PrivateRoute';
+
+// Lazy-loaded components
+const Signup = lazy(() => import('./pages/Signup'));
+const Login = lazy(() => import('./pages/Login'));
+const Dashboard = lazy(() => import('./pages/Dashboard'));
+const UserDetails = lazy(() => import('./pages/UserDetails'));
+const MyPosts = lazy(() => import('./pages/MyPosts'));
+const LikedPosts = lazy(() => import('./pages/LikedPosts'));
+const CreatePosts = lazy(() => import('./pages/CreatePosts'));
+const Posts = lazy(() => import('./pages/Posts'));
+const LandingPage = lazy(() => import('./pages/LandingPage'));
+
 const App = () => {
   return (
     <BrowserRouter>
-    <Navbar/>
-    <Routes>
-      <Route>
-      <Route path="/" element={<Navigate to="/posts" />} />
-        <Route path="/signup" element={<Signup/>} />
-        <Route path="/login" element={<Login/>} />
-        
-        <Route path='/landingpage' element={<LandingPage/>}/>
-        <Route element={<PrivateRoute/>}>
-        <Route path="/dashboard" element={<Dashboard/>} />
-        <Route path="/user/:id" element={<UserDetails/>} />
-        <Route path="/myposts" element={<MyPosts/>}/>
-        <Route path="/likedposts" element={<LikedPosts/>}/>
-        <Route path="/createpost" element={<CreatePosts/>}/>
-        <Route path='/posts' element={<Posts/>}/>
-        
-        </Route>
-        
-      </Route>
-    </Routes>
-    </BrowserRouter>
-  )
-}
+      <Navbar />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Navigate to="/posts" />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/landingpage" element={<LandingPage />} />
 
-export default App
+          <Route element={<PrivateRoute />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/user/:id" element={<UserDetails />} />
+            <Route path="/myposts" element={<MyPosts />} />
+            <Route path="/likedposts" element={<LikedPosts />} />
+            <Route path="/createpost" element={<CreatePosts />} />
+            <Route path="/posts" element={<Posts />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+};
+
+export default App;
